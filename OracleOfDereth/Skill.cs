9800ccddfe4ae93c9686bf1764e45f04fcfa79f1
Advis Eveldan <asheronscall@advis.ca>
2024-10-19 13:48:05 -0600

    Adding CIS and Vitae

--- OracleOfDereth/Skill.cs
+++ OracleOfDereth/Skill.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
-
 using Decal.Adapter;
 using Decal.Adapter.Wrappers;
+using Decal.Filters;
 using VirindiViewService.Controls;
 
 namespace OracleOfDereth
@@ -117,13 +117,21 @@ namespace OracleOfDereth
             int value = CoreManager.Current.CharacterFilter.EffectiveSkill[SkillType];
 
             // TODO: Melee / Missile / Magic augs
-            if(SkillType == CharFilterSkillType.LifeMagic)
+            if (SkillType == CharFilterSkillType.LifeMagic)
             {
                 value += CoreManager.Current.CharacterFilter.GetCharProperty((int)Augmentations.MasterFiveFoldPath) * 10;
             }
 
             // Worlds
             value += CoreManager.Current.CharacterFilter.GetCharProperty((int)Augmentations.AuraWorld);
+
+            // Vitae - This isn't quite correct but it's close enough
+            int vitae = CoreManager.Current.CharacterFilter.Vitae;
+
+            if (vitae > 0)
+            {
+                value -= (int)Math.Round(value * (vitae / 100.0f)) - ((int)Math.Ceiling(vitae / 2.0f) + 1);
+            }
 
             // Jack of All Trades
             value += CoreManager.Current.CharacterFilter.GetCharProperty((int)Augmentations.JackOfAllTrades) * 5;
@@ -140,15 +148,37 @@ namespace OracleOfDereth
                 value += CoreManager.Current.CharacterFilter.GetCharProperty(390);
             }
 
-            // Vitae
-            // value -= VitaeMissing();
+            // CiS
+            if(CoreManager.Current.CharacterFilter.Enchantments.Count(x => (x.SpellId == 5753)) > 0)
+            {
+                value += 20;
+            }
 
             return (value);
         }
+
+        // No Vitae:
+        // Life 536
+        // MeleeD 563
+        // Summon 576
+
+        // 5% Vitae
+        // Life 514
+        // MeleeD 540 (
+        // Summoning 552 (24 less. ingame 21)
+
+        // CIS 5753
+
         public int Vitae()
         {
             return CoreManager.Current.CharacterFilter.Vitae;
         }
+
+        public int VitaeValue()
+        {
+            return (int)Math.Round(CoreManager.Current.CharacterFilter.Vitae / 100.0f);
+        }
+
         public int VitaeMissing()
         {
             if (Vitae() > 0)
