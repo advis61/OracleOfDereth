dd5e4967028da5127764384a89213b2f7f528f34
Advis Eveldan <asheronscall@advis.ca>
2025-09-06 18:02:40 -0600

    Version 1.4.0.0

--- OracleOfDereth/PluginCore.cs
+++ OracleOfDereth/PluginCore.cs
@@ -15,8 +15,8 @@ using WindowsTimer = System.Windows.Forms.Timer;
 [assembly: Guid("153809C7-5D30-12E1-8730-11111104AC1E")]
 
 // Remember to update installer.nsi to match
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 
 namespace OracleOfDereth
 {
